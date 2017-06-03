4d9465a6f86a3950cc7e4051ee4158473ef765e9
Cleverson <cleve@DESKTOP-G0PA5D4>
2017-06-03 19:19:59 -0300

    Adição de Projetos

---
+++ Aula2405_EF_MF/Aula2405_EF_MF/Views/Categorias/Lista.aspx.cs
@@ -0,0 +1,24 @@
+﻿using Aula2405_EF_MF.Controllers;
+using Aula2405_EF_MF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aula2405_EF_MF.Views.Categorias
+{
+    public partial class Lista : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CategoriasController ctrl = new CategoriasController();
+            List<Categoria> lista = ctrl.Listar();
+            List<Categoria> listaExcluidos = ctrl.ListarInativos();
+
+            gvCategorias.DataSource = lista.OrderBy(c => c.Nome);
+            gvCategorias.DataBind();
+
+            gvCategoriasExcluidas.DataSource = listaExcluidos;
+            gvCategoriasExcluidas.DataBind();
+        }
+    }
+}
---
+++ Aula2405_EF_MF/Aula2405_EF_MF/WebForm1.aspx.cs
@@ -0,0 +1,43 @@
+﻿using Aula2405_EF_MF.Controllers;
+using Aula2405_EF_MF.Models;
+using Aula2405_EF_MF.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Aula2405_EF_MF
+{
+    public partial class WebForm1 : BasePage
+    {
+        CategoriasController ctrl = new CategoriasController();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Categoria categoria = new Categoria();
+            categoria.Nome = txtNome.Text;
+            categoria.Descricao = txtDescricao.Text;
+            categoria.Ativo = true;
+
+            ctrl.Adicionar(categoria);
+            Limpar();
+        }
+
+        private void Limpar()
+        {
+            txtDescricao.Text = string.Empty;
+            txtNome.Text = string.Empty;
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Limpar();
+        }
+    }
+}
