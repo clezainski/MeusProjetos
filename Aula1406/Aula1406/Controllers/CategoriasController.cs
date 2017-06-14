0fb25f7ff6ddec2b875035d30ab51da1d27bab0a
Cleverson <cleversonlezainski@gmail.com>
2017-06-14 08:42:42 -0300

    Adição de novos projetos

---
+++ Aula1406/Aula1406/Controllers/CategoriasController.cs
@@ -0,0 +1,32 @@
+﻿using Aula1406.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula1406.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        // GET: Categorias (GET = carregamento da pagina, pela primeira vez)
+        public ActionResult Index()
+        {
+            //Retornar a lista de objetos cadastrados
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        public ActionResult Create(Categoria categoria)
+        {
+            if(model)
+            return View(categoria);
+        }
+    }
+}
---
+++ Aula1406/Aula1406/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Aula1406.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int CategoriaID { get; set; }
+        [Required(ErrorMessage ="Favor preencher o campo nome!")]
+        public string Nome { get; set; }
+        [Display(Name ="Descrição")]
+        public string Descricao { get; set; }
+        [Required]
+        public bool Ativo { get; set; }
+    }
+}
