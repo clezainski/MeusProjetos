b4d80c68cdca1d32aa8e56b5af1d574768226020
Cleverson <cleve@DESKTOP-G0PA5D4>
2017-06-28 11:03:35 -0300

    Finalizado

---
+++ ProjetoFinal/ProjetoFinal/Relatorios/ListaDeUsuarios.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ListaDeUsuarios.aspx.cs" Inherits="ProjetoFinal.Relatorios.ListaDeUsuarios" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=12.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                <ContentTemplate>
+                    <rsweb:ReportViewer ID="ReportViewer" runat="server" Font-Names="Verdana" Font-Size="8pt" WaitMessageFont-Names="Verdana" WaitMessageFont-Size="14pt" Width="804px">
+                        <LocalReport ReportPath="Relatorios\Layouts\ListaUsuarios.rdlc">
+                        </LocalReport>
+                    </rsweb:ReportViewer>
+                </ContentTemplate>
+            </asp:UpdatePanel>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ ProjetoFinal/ProjetoFinal/Relatorios/ListaDeUsuarios.aspx.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Reporting.WebForms;
+using ProjetoFinal.Models;
+using ProjetoFinal.Relatorios.DataSets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjetoFinal.Relatorios
+{
+    public partial class ListaDeUsuarios : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                dsRelatorioDeUsuarios dataSet = new dsRelatorioDeUsuarios();
+
+                Relatorios.DataSets.dsRelatorioDeUsuariosTableAdapters.ListaUsuariosTableAdapter tableAdapter = new Relatorios.DataSets.dsRelatorioDeUsuariosTableAdapters.ListaUsuariosTableAdapter();
+
+                tableAdapter.Fill(dataSet.ListaUsuarios);
+
+
+                ReportViewer.ProcessingMode = Microsoft.Reporting.WebForms.ProcessingMode.Local;
+                ReportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"Relatorios\Layouts\ListaUsuarios.rdlc";
+                ReportViewer.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("DataSet1", (System.Data.DataTable)dataSet.ListaUsuarios));
+
+                ReportViewer.LocalReport.Refresh();
+            }
+        }
+    }
+}
